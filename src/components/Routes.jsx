38565f7f17597d3a9df5dca83c281ae9211206dd
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-10-20 09:34:10 -0700

    Routes done

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Switch, Route, Redirect ,BrowserRouter} from 'react-router-dom';
+
+import { Results } from './Results';
 
 export const Routes = () => {
     return (
-        <div className=''>
-            <h1>Routes</h1>
+        <div className='p-4'>
+            <BrowserRouter>
+            <Switch>
+                <Route exact path='/'>
+                    <Redirect to="/search" />
+                </Route>
+                <Route path={['/search', '/images', '/news', '/videos']}>
+                    <Results />
+                </Route>
+            </Switch>
+            </BrowserRouter>
         </div>
     )
 }
