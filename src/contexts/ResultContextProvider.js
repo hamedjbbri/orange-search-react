d8ba41c4f4ea7e7474ee349011fe6669b5130716
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-10-25 08:51:55 -0700

    Done env and project

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -18,7 +18,7 @@ export const ResultContextProvider = ({ children }) => {
             headers: {
                 'x-user-agent': 'desktop',
     'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-    'x-rapidapi-key': 'f5731df97amshc44b6d20924f92bp170baajsn6d0a7971ea78'
+    'x-rapidapi-key': process.env.REACT_APP_API_KEY,
             }
         });
 
