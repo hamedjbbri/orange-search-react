3ad02077ef12ef955d7cabeba0b52de87106b622
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-10-25 08:21:41 -0700

    navlink tabs done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Links } from './Links';
 
 export const Search = () => {
     return (
         <div>
             Search
+            <Links />
         </div>
     )
 }
